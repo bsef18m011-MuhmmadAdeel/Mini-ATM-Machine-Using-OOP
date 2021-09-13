250108755595c8801f039504ffd59c2fd1e472bb
bsef18m011-MuhammadAdeel <bsef18m011-MuhammadAdeel@users.noreply.github.com>
2021-09-13 23:41:23 +0500

    classes structure added

---
+++ Mini-ATM-Machine/Source.cpp
@@ -0,0 +1,31 @@
+#include<conio.h>
+#include<iostream>
+#include<string>
+using namespace std;
+
+/* Mini Project - ATM
+   -> Check Balance
+   -> Cash withdraw
+   -> User Details
+   -> Update Mobile No.
+*/
+
+
+class ATM										// class atm 
+{
+private:										// private member varibles
+	long int accountNo;
+	string name;
+	int PIN;
+	double balance;
+	string mobileNo;
+
+public:											// public member functions
+
+};
+
+int main()
+{
+
+	return 0;
+}
